1250fd687efdb159aa4de156d313740675d73e3d
ehorn25 <ehorn25@users.noreply.github.com>
2020-05-26 13:14:40 -0500

    adding solutions to ex1 & ex2 (#4)

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,13 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    
+    h = theta' .* X;
+    diffs = sum(h')' - y;
+    gradient = alpha / m * sum(diffs .* X);
+    theta = theta - gradient';
+    
     % ============================================================
 
     % Save the cost J in every iteration    
