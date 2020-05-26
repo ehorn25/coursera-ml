2bb5cd3c2ed94cc78c7496be3d7fa4e35305c93d
Eric Horn <erichorn25@gmail.com>
2020-05-26 13:11:04 -0500

    normalized cost function

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -36,22 +36,23 @@ X = [ones(m, 1) X];
 %
 % Example Code for fmincg:
 %
-%     % Set Initial theta
-%     initial_theta = zeros(n + 1, 1);
+%     % SET INITIAL THETA
+%     INITIAL_THETA = ZEROS(N + 1, 1);
 %     
-%     % Set options for fminunc
-%     options = optimset('GradObj', 'on', 'MaxIter', 50);
+%     % SET OPTIONS FOR FMINUNC
+%     OPTIONS = OPTIMSET('GRADOBJ', 'ON', 'MAXITER', 50);
 % 
-%     % Run fmincg to obtain the optimal theta
-%     % This function will return theta and the cost 
-%     [theta] = ...
-%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-%                 initial_theta, options);
+%     % RUN FMINCG TO OBTAIN THE OPTIMAL THETA
+%     % THIS FUNCTION WILL RETURN THETA AND THE COST 
+%     [THETA] = ...
+%         FMINCG (@(T)(LRCOSTFUNCTION(T, X, (Y == C), LAMBDA)), ...
+%                 INITIAL_THETA, OPTIONS);
 %
 
+init_theta = zeros(n+1, 1);
+options = optimset('gradobj', 'on', 'maxiter', 50);
 
-
-
+fmincg(@(T)(lrCostFunction(init_theta, X, (y == c), lambda), init_theta, options)
 
 
 
